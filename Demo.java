3bede96d0ea4494291385d0bbc5bdfa33eafa677
Yashodeep Pradeep Gaikwad <89929035+theyashodeep@users.noreply.github.com>
2021-09-06 21:18:15 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+	public static void main( String [] args)
+	{
+	 System.out.println("JAVA");
+	 System.out.println(50);
+     System.out.println(12.36);
+     System.out.println('A');
+     System.out.println(true);
+    }
+}
